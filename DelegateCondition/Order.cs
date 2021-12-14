8c12f67eaa638c2ec862e35070f671a9026e84ef
SuriyaSonp <suriya.sonphu@gmail.com>
2021-12-14 13:34:46 +0700

    add files

---
+++ DelegateCondition/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCondition
+{
+    public class Order
+    {
+        public int Quantity { get; set; }
+        public double ItemPrice { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ DelegateCondition/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegateCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new List<Order>()
+            {
+                new Order()
+                {
+                    ProductName = "Apple",
+                    ItemPrice = 30.50,
+                    Quantity = 2
+                },
+                new Order()
+                {
+                    ProductName = "Kiwi",
+                    ItemPrice = 50.25,
+                    Quantity = 12
+                },
+                new Order()
+                {
+                    ProductName = "Watermalon",
+                    ItemPrice = 15.75,
+                    Quantity = 10
+                }
+            };
+
+            var totalPrice = order.Sum(o => o.Quantity * o.ItemPrice);
+
+            var discountPrice = CalculateTotalAfterDiscount(totalPrice);
+
+            Console.WriteLine("***********Your purchased items************");
+
+            foreach (var item in order)
+            {
+                Console.WriteLine(item.ProductName + ", Price/unit:" + item.ItemPrice + ", Quantity: " + item.Quantity + ", Total: " + item.ItemPrice * item.Quantity);
+            }
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Total before discount: " + totalPrice);
+            Console.WriteLine("Discount: " + (totalPrice - discountPrice));
+            Console.WriteLine("Net price: " + discountPrice);
+            Console.WriteLine("*******Thank you for purchasing************");
+            Console.ReadKey();
+        }
+
+        private static double CalculateTotalAfterDiscount(double total)
+        {
+            return SetDelegateCondition().First(s => s.predicate(total)).action(total);
+        }
+
+        private static IList<(Predicate<double> predicate, Func<double, double> action)> SetDelegateCondition()
+        {
+            var cond = new List<(Predicate<double> predicate, Func<double, double> action)>
+            {
+                (
+                (totalPrice) => totalPrice >= 50 && totalPrice < 100,
+                (totalPrice) => totalPrice - (totalPrice * 2) / 100
+                ),
+                (
+                (totalPrice) => totalPrice >= 100 && totalPrice < 500,
+                (totalPrice) => totalPrice - (totalPrice * 5) / 100
+                ),
+                (
+                (totalPrice) => totalPrice >= 500,
+                (totalPrice) => totalPrice - (totalPrice * 10) / 100
+                ),
+                (
+                (totalPrice) => true,
+                (totalPrice) => totalPrice
+                )
+            };
+
+            return cond;
+        }
+    }
+}
